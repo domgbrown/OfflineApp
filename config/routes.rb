b43bf4fe68d619fba55c97c928dc768a5a819ca4
Dominic <dominic@offlinemedia.com>
2012-06-06 00:23:43 -0400

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SampleApp::Application.routes.draw do
 
   get "stat_pages/about"
 
+  get "stat_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/requests/stat_pages_spec.rb
+++ spec/requests/stat_pages_spec.rb
@@ -6,14 +6,20 @@ describe "Static pages" do
 
     it "should have the h1 'Sample App'" do
       visit '/stat_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it "should have the title 'Home'" do
       visit '/stat_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        text: "Ruby on Rails Tutorial Sample App")
     end
+
+    it "should not have a custom page title" do
+    	visit '/stat_pages/home'
+    	page.should_not have_selector('title', text: '| Home')
+    end
+    
   end
 
   describe "Help page" do
