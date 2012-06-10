247cd8c062eb64cf8b3496e3179ac57a1153e2de
Dominic <dominic@offlinemedia.com>
2012-06-10 17:03:41 -0400

    Finish User signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,41 @@ describe "User pages" do
   subject {page}
 
   describe "signup page" do
-    before {visit signup_path}
+    before { visit signup_path }
 
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
+
+  describe "profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before {visit user_path(user)}
+
+  	it { should have_selector('h1', text: user.name) }
+  	it { should have_selector('title', text: user.name) }
+  end
+  
+  describe "signup" do
+  	before { visit signup_path }
+  	let(:submit) { "Create my account" }
+
+  	describe "submit valid signup form" do
+  		before do
+  		fill_in "Name",         with: "Example User"
+  		fill_in "Email",        with: "user@example.com"
+  		fill_in "Password",     with: "foobar"
+  		fill_in "Confirmation", with: "foobar"
+  		end
+  		it "should create user" do
+  			expect { click_button "Create my account" }.to change(User, :count).by(1)
+  		end
+  	end
+
+  	describe "submit blank signup form" do
+  		it "should not create user" do
+  			expect { click_button "Create my account" }.not_to change(User, :count)
+  		end
+  	end
+  end
+
 end
